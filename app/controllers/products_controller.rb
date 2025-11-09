05543eb04cc80dccb5b6b8ce9b652889799f15a8
nedyah212 <hploszay2@academic.rrc.ca>
2025-11-09 13:30:51 -0600

    product card adequately styled for now

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to products_path, alert: "Product not found."
   end
 end
