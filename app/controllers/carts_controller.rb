c8849df435e5fe47b07ee1f8b3a3bbed85786684
nedyah212 <hploszay2@academic.rrc.ca>
2025-11-21 07:58:23 -0600

    updated carts controller to to find products in cart with product_ids (pks) that it receives from session data

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,7 @@
 class CartsController < ApplicationController
   def index
-    @product_ids = session[:cart] || []
+    product_ids = session[:cart] || []
+    @products = Product.where(id: product_ids)
+    end
   end
 end
