059af4b2fee1e6452d3c6067b2ceca73a99a9fa4
nedyah212 <hploszay2@academic.rrc.ca>
2025-11-22 10:36:25 -0600

    addresses are saved in user only page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :create_address, :update_address, :delete_address]
   def show
-    @user = User.find(params[:id])
+    @address = @user.addresses.first || @user.addresses.build
+    @addresses = @user.addresses
+  end
+  def create_address
+    @address = @user.addresses.build(address_params)
+    if @address.save
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+  def update_address
+    @address = @user.addresses.find(params[:address_id])
+    if @address.update(address_params)
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+  def delete_address
+    @address = @user.addresses.find(params[:address_id])
+    @address.destroy
+    redirect_to @user
+  end
+  def set_user
+    @user = User.find(params[:user_id] || params[:id])
+  end
+  def address_params
+    params.require(:address).permit(:street, :city, :postal_code, :province_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :carts, only: [:index]
 
+  resources :users, only: [:show] do
+    post 'addresses', to: 'users#create_address', as: 'addresses'
+    patch 'addresses/:address_id', to: 'users#update_address', as: 'address'
+    delete 'addresses/:address_id', to: 'users#delete_address'
+  end
+
   namespace :admin do
     get 'dashboard', to: 'dashboard#index', as: 'dashboard'
     resources :products, only: [:destroy, :edit, :update, :new, :create]
