aad1ebef870fde2601e528ddec502fc7bc6505e6
nedyah212 <hploszay2@academic.rrc.ca>
2025-11-22 09:48:36 -0600

    recreated admin account to have an addres associated to it for testing

--- db/migrate/20251111225032_drop_admins_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAdminsTable < ActiveRecord::Migration[8.0]
-  def up
-    drop_table :admins
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,21 @@ end
     on_sale: rand(100) < 10
   )
 end
+
+provinces = [
+  { name: "Alberta", code: "AB", gst_rate: 5.0, pst_rate: 0, hst_rate: 0 },
+  { name: "British Columbia", code: "BC", gst_rate: 5.0, pst_rate: 7.0, hst_rate: 0 },
+  { name: "Manitoba", code: "MB", gst_rate: 5.0, pst_rate: 7.0, hst_rate: 0 },
+  { name: "New Brunswick", code: "NB", gst_rate: 0, pst_rate: 0, hst_rate: 15.0 },
+  { name: "Newfoundland and Labrador", code: "NL", gst_rate: 0, pst_rate: 0, hst_rate: 15.0 },
+  { name: "Northwest Territories", code: "NT", gst_rate: 5.0, pst_rate: 0, hst_rate: 0 },
+  { name: "Nova Scotia", code: "NS", gst_rate: 0, pst_rate: 0, hst_rate: 14.0 },
+  { name: "Nunavut", code: "NU", gst_rate: 5.0, pst_rate: 0, hst_rate: 0 },
+  { name: "Ontario", code: "ON", gst_rate: 0, pst_rate: 0, hst_rate: 13.0 },
+  { name: "Quebec", code: "QC", gst_rate: 5.0, pst_rate: 9.975, hst_rate: 0 },
+  { name: "Prince Edward Island", code: "PE", gst_rate: 0, pst_rate: 0, hst_rate: 15.0 },
+  { name: "Saskatchewan", code: "SK", gst_rate: 5.0, pst_rate: 6.0, hst_rate: 0 },
+  { name: "Yukon", code: "YT", gst_rate: 5.0, pst_rate: 0, hst_rate: 0 }
+]
+
+provinces.each { |province| Province.create!(province) }
